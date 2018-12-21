82f4eff09a0afc22ea7eb0e446a38eb86f3e2eb2
e2tox <e2tox@live.com>
2018-12-21 12:18:06 +0800

    1

--- index.ts
+++ index.ts
@@ -1,26 +1,42 @@
 // 1. import
 import { mandatory, optional, Validate } from '@agentframework/validation';
 
-// 2. define class using AgentFramework way
+// 2. define data models
+class Project {
+  @mandatory()
+  name: string;
+}
+
 class User {
   @mandatory()
   id: number;
-  
+
   @mandatory()
   name: string;
-  
+
   @optional()
   email: string;
+
+  @optional()
+  project: Project;
 }
 
-// 3. The object to validate
+// 3. Create validator
+const Validator = Validate(User);
+
+// 4. The object to validate
 const input = {
   id: 1,
   name: 'Ling'
 };
-
-// 3. Create validator for the class and test the result
-
-const Validator = Validate(User);
-
 console.log('is match?', Validator.test(input));
+
+// 5. The object to validate
+const inputWithProject = {
+  id: 1,
+  name: 'Ling',
+  project: {
+    name: 1
+  }
+};
+console.log('project validate result?', Validator.validate(inputWithProject));
